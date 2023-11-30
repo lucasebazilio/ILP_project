b48f8eff190eef81f701575aba912bfb45a24691
AdriaCoding <adrialisa99@gmail.com>
2023-11-30 19:01:31 +0100

    Algunas instancias

--- instancegen.py
+++ instancegen.py
@@ -3,19 +3,20 @@ import random
 
 def generate_instance(file_path, nOrders, nSlots, p, l, c, mindi, maxdi, maxsur):
     with open(file_path, 'w') as file:
-        file.write(f'nOrders = {nOrders}\n')
-        file.write(f'nSlots = {nSlots}\n')
-        file.write(f'p = {p}\n')
-        file.write(f'l = {l}\n')
-        file.write(f'c = {c}\n')
-        file.write(f'mindi = {mindi}\n')
-        file.write(f'maxdi = {maxdi}\n')
-        file.write(f'maxsur = {maxsur}\n')
+        file.write(f'nOrders={nOrders};\n')
+        file.write(f'nSlots={nSlots};\n')
+        file.write(f'p={p};\n')
+        file.write(f'l={l};\n')
+        file.write(f'c={c};\n')
+        file.write(f'mindi={mindi};\n')
+        file.write(f'maxdi={maxdi};\n')
+        file.write(f'maxsur={maxsur};\n')
 
 def compute_values(nOrders, nSlots):
     p = [random.uniform(0, 200) for _ in range(nOrders)]
-    l = random.sample(range(1, nSlots), nOrders)
-    maxsur = random.uniform(10.0, 80.0)
+    l = [random.randint(1, nSlots) for _ in range(nOrders)]
+    #maxsur = random.uniform(10.0, 80.0)
+    maxsur = 50.0
     c = [random.uniform(1.0, maxsur) for _ in range(nOrders)]
     mindi =[random.randint(l[i], nSlots) for i in range(nOrders)]
     maxdi =[random.randint(mindi[i], nSlots) for i in range(nOrders)]
@@ -30,9 +31,8 @@ def compute_values(nOrders, nSlots):
     generate_instance(file_path, nOrders, nSlots, p, l, c, mindi, maxdi, maxsur)
 
 def generate_massive(N):
-    for i in range(N):
-        compute_values(100*i, 100*i)
+    for i in range(1,N):
+        compute_values(100*i, 12)
 
 if __name__ == "__main__":
-    
-    compute_values(4, 7)
+    generate_massive(10)
