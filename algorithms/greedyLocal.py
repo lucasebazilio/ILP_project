9d957bdb31b05147cd3c09c34d33f6df7037a888
Lucas Bazilio <45127111+lucasebazilio@users.noreply.github.com>
2023-12-07 15:02:08 +0100

    Local Search

--- algorithms/greedyLocal.py
+++ algorithms/greedyLocal.py
@@ -1,27 +1,12 @@
 from collections import Counter
-import configparser
-from configparser import ConfigParser, ExtendedInterpolation
+from input_initialization import initialize_input
 from solution import Sol
 
-config = configparser.ConfigParser()
-config.read('sampleInstance.dat')
+# Specify your .dat file
+dat_file = 'instance_n200_t12.dat'
 
-# Access values using the section and option
-nOrders = config.getint('Values', 'nOrders')
-nSlots = config.getint('Values', 'nSlots')
-p = config.get('Values', 'p')
-l = config.get('Values', 'l')
-c = config.get('Values', 'c')
-mindi = config.get('Values', 'mindi')
-maxdi = config.get('Values', 'maxdi')
-maxsur = config.getfloat('Values', 'maxsur')
-
-# Parse string lists into actual lists
-p = [float(val) for val in p.strip('[]').split(',')]
-l = [int(val) for val in l.strip('[]').split(',')]
-c = [float(val) for val in c.strip('[]').split(',')]
-mindi = [int(val) for val in mindi.strip('[]').split(',')]
-maxdi = [int(val) for val in maxdi.strip('[]').split(',')]
+# Initialize input
+nOrders, nSlots, p, l, c, mindi, maxdi, maxsur = initialize_input(dat_file)
 
 sol_instance = Sol(nOrders, nSlots, p, l, c, mindi, maxdi, maxsur)
 
